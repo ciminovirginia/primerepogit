b347aa03ac6c13bad3d4cbcfa38edd8121dca329
vcimino6 <prueba@correo.com>
2025-03-11 18:32:55 -0300

    se uso input

---
+++ ejercicio2.py
@@ -0,0 +1,4 @@
+nombre=input("ingresa tu nombre")
+apellido=input("ingresa tu apellido")
+materia="programacion 1"
+print(f"mi nombre es nombre es  {nombre} y mi apellido es {apellido} dicto {materia}")
