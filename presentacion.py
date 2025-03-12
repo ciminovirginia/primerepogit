83595613397ff5ae3950bbcae14ca583a1780da0
vcimino6 <prueba@correo.com>
2025-03-12 10:22:48 -0300

    comentarios kklkkk

--- presentacion.py
+++ presentacion.py
@@ -1,4 +1,8 @@
+#se define nombre
 nombre="virginia"
+#se define apellido
 apellido="cimino"
+# se define materia
+#klklklklklklklklklklklklk
 materia="programacion 1"
-print(f"mi nombre es nombre{nombre} y mi apellido es {apellido} dicto {materia}")
+print(f"mi nombre es nombre es  {nombre} y mi apellido es {apellido} dicto {materia}")
